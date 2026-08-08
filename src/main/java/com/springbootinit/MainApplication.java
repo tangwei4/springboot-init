71709899e4c60ffb3d1ec12498303f3d2147073d
cciocfan <2424176683@qq.com>
2026-08-08 18:36:17 +0800

    生成基本crud 提供给后续测试

--- src/main/java/com/springbootinit/MainApplication.java
+++ src/main/java/com/springbootinit/MainApplication.java
@@ -3,14 +3,13 @@ package com.springbootinit;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 主类（项目启动入口）
  */
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.springbootinit.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
